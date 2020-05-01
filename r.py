7f0645fb2c6c4cf08b7cd96d639a35eac9f722b7
JCChiou <rex2763@gmail.com>
2020-05-01 23:13:45 +0800

    average len

--- r.py
+++ r.py
@@ -1,11 +1,11 @@
 #read file
 data = []
+avg_rew = 0
 count = 0
 with open('original.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-		count += 1
-		if count % 1000 == 0:
-			print(len(data))
-
-
+print('檔案讀取完畢,總共有', len(data), "筆資料")
+for e_review in data:
+	avg_rew += len(e_review)
+print(avg_rew / len(data))
